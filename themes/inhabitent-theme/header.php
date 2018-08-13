2e18e4b77240166321df17743ce9c1af3343c186
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-08-13 14:41:30 -0700

    added search field and icon, added new variable for nav color

--- themes/inhabitent-theme/header.php
+++ themes/inhabitent-theme/header.php
@@ -6,24 +6,24 @@
  */
 
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes();?>>
 	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta charset="<?php bloginfo('charset');?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<link rel="pingback" href="<?php bloginfo('pingback_url');?>">
 
-	<?php wp_head(); ?>
+	<?php wp_head();?>
 	</head>
 
-	<body <?php body_class(); ?>>
+	<body <?php body_class();?>>
 		<div id="page" class="hfeed site">
-			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
+			<a class="skip-link screen-reader-text" href="#content"><?php esc_html('Skip to content');?></a>
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name');?></a></h1>
+					<p class="site-description"><?php bloginfo('description');?></p>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
@@ -31,12 +31,17 @@
 					<div class="white-logo-container">
 
 				<a>
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/inhabitent-logo-tent-white.svg">	
-				
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/inhabitent-logo-tent-white.svg">
+
 </div>
 
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html('Primary Menu');?></button>
+					<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu'));?>
+					<form class="search">
+						<input type="text">
+						<i class="fas fa-search" href="#"></i>
+						</input>
+				</form>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
