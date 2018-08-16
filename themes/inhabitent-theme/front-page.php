f815e0ee62f3b3d5f9a2ae8febd1262840ef21cd
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-08-15 17:06:56 -0700

    added front-page styles

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -1,70 +1,87 @@
 <?php get_header(); ?>
 
-<div class="hero-banner">
+<section class="front-page-wrapper">
 
-<div class="main-logo-container">
+	<div class="hero-banner">
+
+	<div class="main-logo-container">
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/inhabitent-logo-full.svg">			
-</div> 
+	</div> 
 
-</div>
+	</div> <!-- end of .hero-banner -->
 
-<div class="home-terms-container">
-<?php
-	 $terms = get_terms (array( 
+	<div class="home-shop-stuff">
+		<h2> shop stuff </h2>
+	</div>
+
+	<?php
+	 	$terms = get_terms (array( 
 		'taxonomy' => 'product_type'
-	 ));
-?>
-<?php foreach ( $terms as $term ):  ?>
+	 	));
+		?>
 
-<div class="home-images-container">
-	<img src="<?php echo get_template_directory_uri()?>/assets/images/<?php echo $term->name?>.svg">
-</div>
+	
+		<?php foreach ( $terms as $term ):  ?>
+		<div class="home-terms-container">
+			 
+			<img src="<?php echo get_template_directory_uri()?>/assets/images/<?php echo $term->name?>.svg">
+	
+	
+			<p><?php echo $term->description; ?></p>
 
-<div class="home-description">
-	<p><?php echo $term->description; ?></p>
-</div>
+			<a href="#">
+			<h3><?php echo $term->name . " stuff"; ?></h3>
+			</a>
 
-<div class="home-term-name">
-	<h2><?php echo $term->name . " stuff"; ?></h2>
-</div>
+		</div> <!-- end of .home-terms-container -->
 
-<!-- <?php print_r($term) ?> shows what my options are-->
+	<!-- <?php print_r($term) ?> shows what my options are-->
 
-<?php endforeach; ?>
-</div>
+		<?php endforeach; ?>
 
 
-<?php
+	<div class="home-shop-stuff">
+		<h2> inhabitent journal </h2>
+	</div>
+
+	
+	<?php
    $args = array( 
 		 'post_type' => 'post', 
 		 'order' => 'DSC',
 		 'numberposts' => 3
 		);
    $product_posts = get_posts( $args ); // returns an array of posts
-?>
-<?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+	?>
+	<?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+	
+	<div class="home-posts-container">
 
-<div class="home-posts-container">
+	<div class="home-image-container">
+		<?php	 the_post_thumbnail( 'large' ); ?>
+	</div>
 
-<div class="home-image-container">
-	<?php	 the_post_thumbnail( 'large' ); ?>
-</div>
 
-<div class="home-meta-container"> 
-	<?php inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_posted_by(); ?>
-</div>
 
-<div class="journal-content">
-	<h3><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-</div>
+	<div class="home-meta-container"> 
+		<?php inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_posted_by(); ?>
+	</div>
 
-<div class="home-readmore">
-<span><a href="<?php the_permalink(); ?>">Read Entry</a></span>
-</div>
+	
+	<h3 class="journal-content">
+		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+	</h3>
 
-</div> <!-- end of .home-posts-container -->
-			
-<?php endforeach; wp_reset_postdata(); ?>
 
+	<span class="home-readmore" >
+		<a href="<?php the_permalink(); ?>">Read Entry</a>
+	</span>
+
+	</div> <!-- end of .home-posts-container -->
+	<?php endforeach; wp_reset_postdata(); ?>
+
+
+
+</section>
 
 <?php get_footer(); ?>
