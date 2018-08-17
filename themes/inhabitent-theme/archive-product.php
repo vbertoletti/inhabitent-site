d41c6ccc94fac6a1747ba283304982be27e72b60
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-08-16 20:39:37 -0700

    Applied styles on shop page grid

--- themes/inhabitent-theme/archive-product.php
+++ themes/inhabitent-theme/archive-product.php
@@ -5,7 +5,7 @@
  * @package RED_Starter_Theme
  */
 
-get_header(); ?>
+	get_header(); ?>
 
 	<div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
@@ -15,17 +15,15 @@ get_header(); ?>
 		</header><!-- .shop-header -->
 
 
-  <div class="products-section-wrapper">
+		<!-- getting terms to show -->
+  	<div class="products-section-wrapper">
 
 		<?php if ( have_posts() ) : ?>
-
-	<!-- loop over terms and echo them to the screen -->
 			<?php
 	 			$terms = get_terms (array( 
 				'taxonomy' => 'product_type',
 	 			));
 			?>
-
 			<?php foreach ( $terms as $term ):  ?>
 				 
 				<section class="terms-container">
@@ -37,8 +35,9 @@ get_header(); ?>
 			</section>
 			</div>
 	
-			<?php /* Start the Loop on products */ ?>
-
+			<!--loop on products -->
+				 
+			<div class="shop-products">
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -49,9 +48,9 @@ get_header(); ?>
 											
 						<div class="product-related-info">
 
-						<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+						<?php the_title( '<h3>', '</h3>' ); ?>
 		
-						<p class="price"> ................. <?php echo CFS()->get( 'price' ); ?></p>
+						<p class="price"> ............. <?php echo CFS()->get( 'price' ); ?></p>
 
 						</div>
 
