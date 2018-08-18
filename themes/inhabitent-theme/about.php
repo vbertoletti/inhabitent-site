e36170114024da05d16a4d90a2d597acf7ade441
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-08-18 14:00:12 -0700

    Worked on sidebar

--- themes/inhabitent-theme/about.php
+++ themes/inhabitent-theme/about.php
@@ -11,28 +11,25 @@ get_header(); ?>
 	<div id="primary" class="about-content-area">
 		<main id="main" class="site-main" role="main">
 
-			<section class="about-wrapper">
-
 			<?php while ( have_posts() ) : the_post(); ?>
 
 			 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
+		
+				<?php the_content(); ?>
+				<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
 				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+				) );
+			?>
+	
+		</div><!-- .entry-content -->
+		</article><!-- #post-## -->
 
 
 			<?php endwhile; ?>
 
-
-			</section>
 
 		</main> 
 	</div>
