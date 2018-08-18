562597b84ee3f798bcb61cfe9af93c0b3f14a143
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-08-18 13:01:46 -0700

    Worked on single-product.php

--- themes/inhabitent-theme/single-product.php
+++ themes/inhabitent-theme/single-product.php
@@ -7,32 +7,43 @@
 
 get_header();?>
 
-	<div id="primary" class="content-area">
+		<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php while (have_posts()): the_post();?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID();?>" <?php post_class();?>>
-		<header class="entry-header">
-			<?php if (has_post_thumbnail()): ?>
-				<?php the_post_thumbnail('large');?>
-			<?php endif;?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<?php the_title('<h1 class="entry-title">', '</h1>');?>
+		<header>
 
-	</header><!-- .entry-header -->
+			<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail( 'large' ); ?>
+			<?php endif; ?>
 
-	<div class="entry-content">
-  <?php echo CFS()->get( 'price' ); ?>
-		<?php the_content();?>
-	</div><!-- .entry-content -->
+		</header>
 
-</article><!-- #post-## -->
 
+		<div class="single-page-content">
+		<?php the_title( '<h1 class="entry-title-single">', '</h1>' ); ?>
+		<div class="price-container">
+		<?php echo CFS()->get( 'price' ); ?>
+		</div>
+
+		<?php the_content(); ?>
+
+				<span class="social-media"> <i class="fab fa-facebook"></i> Like </span>
+				<span class="social-media"><i class="fab fa-twitter"></i> Tweet </span>
+				<span class="social-media"><i class="fab fa-pinterest"></i> Pin </span>
+
+
+		</div><!-- .entry-content -->
+		
+		</article><!-- #post-## -->
+	
 
 		<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
+	
+	</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php get_footer();?>
