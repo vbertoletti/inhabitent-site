65abd60602beb1c8a042ce66b77148182c523f5d
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-08-19 21:45:12 -0700

    Worked on archive.php styles

--- themes/inhabitent-theme/archive.php
+++ themes/inhabitent-theme/archive.php
@@ -8,14 +8,14 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="archive-site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
-
+		
 			<header class="page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+					the_archive_title( '<h1 class="archive-page-title">', '</h1>' );
+					the_archive_description( '<div class="archive-taxonomy-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
