71100d1cc89c3c09768cda3faa14530e6aa9ad65
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-08-19 18:47:41 -0700

    Worked on blog styles and 404 page

--- themes/inhabitent-theme/404.php
+++ themes/inhabitent-theme/404.php
@@ -20,7 +20,9 @@ get_header(); ?>
 				<div class="page-content">
 					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?' ); ?></p>
 
+					<div class="error-form">
 					<?php get_search_form(); ?>
+					</div>
 
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
@@ -51,5 +53,5 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+	<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- themes/inhabitent-theme/single.php
+++ themes/inhabitent-theme/single.php
@@ -8,22 +8,34 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="single-page-main" class="site-main" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
+		<div class="single-page-blog-container">
+
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<?php the_post_navigation(); ?>
+				<div class="single-page-social-media">
+				<span class="social-media"> <i class="fab fa-facebook"></i> Like </span>
+				<span class="social-media"><i class="fab fa-twitter"></i> Tweet </span>
+				<span class="social-media"><i class="fab fa-pinterest"></i> Pin </span>
+				</div>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
+
 					comments_template();
+					
 				endif;
 			?>
 
+
+			
+
 		<?php endwhile; // End of the loop. ?>
+		</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
