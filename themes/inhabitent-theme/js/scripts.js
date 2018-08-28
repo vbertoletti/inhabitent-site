03b0a378bf866f8344c485cc15e3e702ae636c79
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-08-28 15:32:38 -0700

    Simplified js file, added js scroll function to change nav bar for home and about page past the hero banner.

--- themes/inhabitent-theme/js/scripts.js
+++ themes/inhabitent-theme/js/scripts.js
@@ -1,21 +1,39 @@
 jQuery(document).ready(function($) {
 
-  $('.search-section-nav').on('click', function(event) {
 
+  $('.search-section-nav').on('click', function(event) {
+    
     // The default action that belongs to the event will not occur. Clicking on the submit button, prevents it from re-directing to the search page.
     event.preventDefault();
 
-    //Toggles the serach input field
-    $('.search-field').addClass('search-active');
-    // you can also try toggleClass or addClass and removeClass
+    var $searchField = $('.search-field');
 
     //When clicking the icon, focus will be on the input field isntead of icon
-    $('.search-field').focus();
+    $searchField.addClass('search-active').focus();
 
     //When it loses focus by clicking somewhere else, hides the input field
-    $('.search-field').on('blur', function(){
-        $('.search-field').toggleClass('search-active');
+    $searchField.on('blur', function() {
+      $searchField.removeClass('search-active');
     });
-
   });
+
+
+
+  // Found below example to change nav menu color once user scrolls below the hero banner.
+  // http://lukedowding.com/change-nav-bar-menu-add-css-class-scroll/
+
+  $(function() {
+    var header = $('#site-navigation');
+
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll >= 700) {
+        header.addClass('scrolled');
+      } else {
+        header.removeClass('scrolled');
+      }
+    });
+  });
+
+
 });
